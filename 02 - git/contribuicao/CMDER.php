4faec7e39ad58c4bd87744f7591dd9487c16b7e0
Vanderleia de Martins Cavalho <vanderleia.carvalho2@estudante.ifto.edu.br>
2020-09-09 08:34:31 -0300

    cmder

---
+++ 02 - git/contribuicao/CMDER.php
@@ -0,0 +1,8 @@
+{
+"terminal.integrated.shell.windwos": "c:\\WINDOWS\\system32\\cmd.exe",
+"terminal.integrated.shellArgs.windows": [
+"/k",
+"c:\\cmder\\vendor\\bin\\vscode_init.cmd"
+],
+"files.autoSave": "onFocusChange"
+},
