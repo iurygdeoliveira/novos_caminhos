365774850106d001842ca8fc0375c64f140fcf2e
José Alexandre Rabelo da Silva <alexmsn077@gmail.com>
2020-09-11 12:32:08 -0300

    atualizações 11-09

---
+++ www/assets/index.php
@@ -0,0 +1,22 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang='pt-br'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Document</title>
+</head>
+<body>
+ 
+</body>
+</html>
+
+<?php
+require __DIR__ . '/../framework/config.php';
+PHPClassName('CLASS_TITLE');
+ 
+/*
+*
+*/
+PHPClassSession('', __LINE__);
