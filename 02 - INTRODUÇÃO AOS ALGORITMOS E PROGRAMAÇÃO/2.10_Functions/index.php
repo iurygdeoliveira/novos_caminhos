be7a8aa5893234b3c8a008c6b27b560d182a1eba
Thiago Sartori <thiagosartori@live.com>
2020-09-17 21:00:07 -0300

    Atualizando functions

--- 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/2.10_Functions/index.php
+++ 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/2.10_Functions/index.php
@@ -2,36 +2,40 @@
 require __DIR__ . '/../framework/config.php';
 PHPClassName('Aula 10 - Trabalhando com funções');
 
+
+PHPClassSession('functions', __LINE__);
+
 require __DIR__ . '/functions.php';
 
-PHPClassSession("functions", __LINE__);
+var_dump(functionName("Thiago Sartori", "Developer", "Programador WEB"));
 
-var_dump(functionName("IFTO", "NOVOS CAMINHOS", 'PROGRAMADOR WEB'));
-var_dump(functionName("Iury", "Pedrinha", 'Jade'));
+var_dump(calcIMC(90, 1.93));
+var_dump(calcIMC(100, 1.93));
+var_dump(calcIMC(113, 1.93));
 
-// var_dump(calcIMC(86, 1.72));
-// var_dump(calcIMC(90, 1.89));
-// var_dump(calcIMC(100, 2));
+PHPClassSession('global access', __LINE__);
 
-PHPClassSession("global access", __LINE__);
-
-
-$weight = 91;
-$height = 1.90;
+$weight = 90;
+$height = 1.93;
 
 var_dump(calcIMC_Global());
 
-PHPClassSession("static arguments", __LINE__);
+PHPClassSession('static arguments', __LINE__);
 
-$pay = payTotal(200);
+$pay = payTotal(470);
 $pay = payTotal(150);
 $pay = payTotal(500);
 
 var_dump($pay);
+print_r($pay);
+// O valor retornado será de 1120 pois a variável static não perde o
+// valor quando o método é chamado novamente, ou seja, a variável não
+// perde o valor depois que o método foi completamente executado,
+// e quando ele é executado novamente, a variável já está com um valor
+// armazenado de execuções anteriores.
 
-PHPClassSession("dinamic arguments", __LINE__);
+PHPClassSession('dinamic arguments', __LINE__);
 
 var_dump(myTeam());
-var_dump(myTeam("Fernando"));
-var_dump(myTeam("Fernando", "Iury"));
-var_dump(myTeam("Fernando", "Iury", "Daniel"));
+var_dump(myTeam("Thiago"));
+var_dump(myTeam("Iury", "Daniel", "Chupa-Cabra"));
