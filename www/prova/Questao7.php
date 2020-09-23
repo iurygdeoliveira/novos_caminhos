f489a05ea49d50b8ff60218fbeec6e695a3802f0
Iury Gomes de Oliveira <iury.oliveira@ifto.edu.br>
2020-09-23 09:24:57 -0300

    Adicionando Prova

---
+++ www/prova/Questao7.php
@@ -0,0 +1,15 @@
+<?php
+
+// Questão 7
+// Dadas duas strings, encontre o
+// número de caracteres comuns entre elas.
+
+//Exemplo
+
+//Para $s1 = "aabcc"e $s2 = "adcaa", o retorno deve ser 3.
+//As strings têm 3 caracteres comuns 2 "a" e 1"c".
+
+function commonCharacterCount($s1, $s2)
+{
+    // CÓDIGO
+}
