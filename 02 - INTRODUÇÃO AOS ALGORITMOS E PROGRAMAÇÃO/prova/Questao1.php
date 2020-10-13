c6ae50492ca6ba2063cfe46a8fd00770d23cbfef
Iury Gomes de Oliveira <iury.oliveira@ifto.edu.br>
2020-10-13 12:59:39 -0300

    prova respondida

---
+++ 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/prova/Questao1.php
@@ -0,0 +1,28 @@
+<?php
+
+// Questão 1
+// Considere a function incompleta abaixo e escreva um código que
+// resolva o seguinte problema
+
+// Dado um ano, retorne o século em que ele se encontra.
+// O primeiro século se estende do ano 1 até e incluindo o ano 100,
+// o segundo segundo século do ano 101 até e incluindo o ano 200,
+// e assim sucessivamente.
+
+//Exemplo
+
+//Para $ano = 1905, a saída deve ser
+//centuryFromYear(year) = 20;
+//Pois year = 1700, a saída deve ser
+//centuryFromYear(year) = 17.
+
+//Retorno inteiro
+//O número do século em que se encontra o ano.
+
+
+function centuryFromYear($year)
+{
+
+    // CÓDIGO ...
+
+}
