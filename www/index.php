0abb70c789801c6d45153400bc73a160353862fe
Pedro Lennon <pedro21900@hotmail.com>
2020-10-29 22:57:04 -0300

    Conceitos html e php

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='robots' content='index, follow'>
+    <link rel='shortcut icon' href=''>
+    <link rel='stylesheet' href=''>
+    <title>Docum</title>
+</head>
+
+<body>
+    <?php 
+    /**Bloco PHP com variavel start
+     * E comando de saida
+     *  */ 
+    $start = "Vamos iniciar!";
+    echo "<h1>Olá Mundo{$start}</h1>";
+    echo "<p id='js'>Loging.......</p>";
+    ?>
+    <script>
+    </script>
+</body>
+
+</html>
